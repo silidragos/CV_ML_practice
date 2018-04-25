267355d5c1cc94415d6098ca628fc795ef224afa
Dragos Silion <silidragos@gmail.com>
2018-04-25 21:46:00 +0300

    Tips

---
+++ programming_tips/good_to_know.py
@@ -0,0 +1,14 @@
+##RULES
+#Debugging in ML is often data debugging rather than code debugging.
+
+#Matplotlib
+#Show Multiple plots at once
+plt.subplot(1,2,2) 
+
+#Pandas
+##Panda Display Settings
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+
+
